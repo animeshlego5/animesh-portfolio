fea9dd5ab96fccafd57b24567e8b6362c1a3a7c6
Animesh Gosain <126562373+animeshlego5@users.noreply.github.com>
2026-01-12 23:14:58 +0530

    Refine AG mark with user provided SVG and update layout

--- src/components/ag-mark.tsx
+++ src/components/ag-mark.tsx
@@ -2,19 +2,22 @@ export function AgMark(props: React.ComponentProps<"svg">) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
             fill="none"
-            viewBox="0 0 512 256"
             {...props}
         >
             <path
                 fill="currentColor"
-                d="M64 0h128v64h-128zM0 64h64v192h-64zM192 64h64v192h-64zM64 128h128v64h-128zM320 0h128v64h-128zM256 64h64v128h-64zM320 192h128v64h-128zM448 128h64v64h-64zM384 128h64v64h-64z"
+                d="M80 144h128v32H80zm-32 32h64v192H48zm128 0h64v192h-64zm-64 64h64v32h-64z"
+            />
+            <path
+                fill="currentColor"
+                d="M304 144h128v32H304zm-32 32h64v160h-64zm32 160h128v32H304zm96-160h64v32h-64zm-32 64h96v32h-96zm32 32h64v64h-64z"
             />
         </svg>
     );
 }
 
 export function getAgMarkSVG(color: string) {
-    // Simplified path for smaller viewbox 256x128 (0.5 scale)
-    return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M32 0h64v32h-64zM0 32h32v96h-32zM96 32h32v96h-32zM32 64h64v32h-64zM160 0h64v32h-64zM128 32h32v64h-32zM160 96h64v32h-64zM224 64h32v32h-32zM192 64h32v32h-32z"/></svg>`;
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="none"><path fill="${color}" d="M80 144h128v32H80zm-32 32h64v192H48zm128 0h64v192h-64zm-64 64h64v32h-64z"/><path fill="${color}" d="M304 144h128v32H304zm-32 32h64v160h-64zm32 160h128v32H304zm96-160h64v32h-64zm-32 64h96v32h-96zm32 32h64v64h-64z"/></svg>`;
 }
