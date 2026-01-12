06f7a8b013334bb08390356601100d99b47b0cea
Animesh Gosain <126562373+animeshlego5@users.noreply.github.com>
2026-01-12 23:48:47 +0530

    Resize AG mark to match original 2:1 aspect ratio

--- src/components/ag-mark.tsx
+++ src/components/ag-mark.tsx
@@ -2,7 +2,7 @@ export function AgMark(props: React.ComponentProps<"svg">) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
+            viewBox="48 144 416 224"
             fill="none"
             {...props}
         >
@@ -19,5 +19,5 @@ export function AgMark(props: React.ComponentProps<"svg">) {
 }
 
 export function getAgMarkSVG(color: string) {
-    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="none"><path fill="${color}" d="M80 144h128v32H80zm-32 32h64v192H48zm128 0h64v192h-64zm-64 64h64v32h-64z"/><path fill="${color}" d="M304 144h128v32H304zm-32 32h64v160h-64zm32 160h128v32H304zm96-160h64v32h-64zm-32 64h96v32h-96zm32 32h64v64h-64z"/></svg>`;
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="48 144 416 224" fill="none"><path fill="${color}" d="M80 144h128v32H80zm-32 32h64v192H48zm128 0h64v192h-64zm-64 64h64v32h-64z"/><path fill="${color}" d="M304 144h128v32H304zm-32 32h64v160h-64zm32 160h128v32H304zm96-160h64v32h-64zm-32 64h96v32h-96zm32 32h64v64h-64z"/></svg>`;
 }
